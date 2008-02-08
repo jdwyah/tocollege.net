829caf5b7cf7813906d44b67761dad5c7f15512b
Jeff Dwyer <jdwyah@gmail.com>
2008-02-08 23:42:34 +0000

    attempt to fix weird linux only ImageBundle compliation problem.

--- src/main/java/com/apress/progwt/client/consts/images/Images.java
+++ src/main/java/com/apress/progwt/client/consts/images/Images.java
@@ -13,34 +13,78 @@ import com.google.gwt.user.client.ui.ImageBundle;
  */
 public interface Images extends ImageBundle {
 
+    /**
+     * @gwt.resource magnifyingBig.png
+     */
     AbstractImagePrototype magnifyingBig();
 
+    /**
+     * @gwt.resource magnifyingSmall.png
+     */
     AbstractImagePrototype magnifyingSmall();
 
+    /**
+     * @gwt.resource bullet_blue.png
+     */
     AbstractImagePrototype bullet_blue();
 
+    /**
+     * @gwt.resource applying.png
+     */
     AbstractImagePrototype applying();
 
+    /**
+     * @gwt.resource accepted.png
+     */
     AbstractImagePrototype accepted();
 
+    /**
+     * @gwt.resource rejected.png
+     */
     AbstractImagePrototype rejected();
 
+    /**
+     * @gwt.resource applied.png
+     */
     AbstractImagePrototype applied();
 
+    /**
+     * @gwt.resource considering.png
+     */
     AbstractImagePrototype considering();
 
+    /**
+     * @gwt.resource pctStatus0.png
+     */
     AbstractImagePrototype pctStatus0();
 
+    /**
+     * @gwt.resource pctStatus25.png
+     */
     AbstractImagePrototype pctStatus25();
 
+    /**
+     * @gwt.resource pctStatus50.png
+     */
     AbstractImagePrototype pctStatus50();
 
+    /**
+     * @gwt.resource pctStatus75.png
+     */
     AbstractImagePrototype pctStatus75();
 
+    /**
+     * @gwt.resource pctStatus100.png
+     */
     AbstractImagePrototype pctStatus100();
 
+    /**
+     * @gwt.resource checked.png
+     */
     AbstractImagePrototype checked();
 
+    /**
+     * @gwt.resource unchecked.png
+     */
     AbstractImagePrototype unchecked();
-
 }
